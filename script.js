5e27ab7d9d0dcf7abdba999a9a0765bdfa995b0e
Kaushal <Kaushal@LAPTOP-M67835KB.(none)>
2026-03-31 19:14:59 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,13 @@
+const btnRock = document.querySelector('.btnRock').addEventListener('click', playGame);
+const btnPaper = document.querySelector('.btnPaper').addEventListener('click', playGame);
+const btnScissors = document.querySelector('.btnScissors').addEventListener('click', playGame);
+const btnRestart = document.querySelector('.btnRestart').addEventListener('click', restartGame);
+const computerChoice = Math.random();
+
+
+
+
+
+const playGame = () => {
+    if()
+}
