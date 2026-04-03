778c15a81a76454ed683e032c895329c8bc6e62b
Kaushal <Kaushal@LAPTOP-M67835KB.(none)>
2026-04-03 16:52:53 +0530

    updated

--- script.js
+++ script.js
@@ -1,12 +1,27 @@
 
+const chosen = document.querySelector('.chosen');
+const win = document.querySelector('.win');
+const lose = document.querySelector('.lose');
+const tie = document.querySelector('.tie');
+const yourScore = document.querySelector('.yourScore');
+const computerScore = document.querySelector('.computerScore');
+let result = '';let wincount = 0; let losecount = 0; let tiecount = 0;
 
-let result = '';
+const score = JSON.parse(localStorage.getItem('score'))||{
+    playercount : 0,
+    compcount : 0
+};
+win.innerText = `Wins: ${wincount}`;
+lose.innerText = `Loses: ${losecount}`;
+tie.innerText = `Ties: ${tiecount}`;
+yourScore.innerText = `Your Score: ${score.playercount}`;
+computerScore.innerText = `Computer Score: ${score.compcount}`;
 
 // to show choice of both player and computer via emogis
-const chosen = document.querySelector('.chosen');
 const comIcon = (player, computerChoice) => {
     const div1 = document.createElement('div');
     const div2 = document.createElement('div');
+
     div1.style.backgroundImage = `url('images/${player}-emoji.png')`;
     div1.style.width = "50px";
     div1.style.height = "50px";
@@ -29,12 +44,11 @@ const comIcon = (player, computerChoice) => {
     p2.style.opacity = "0.3";
     div2.appendChild(p2);
     
-
     chosen.innerHTML = '';
     chosen.appendChild(div1);
-    chosen.appendChild(div2);
-        
+    chosen.appendChild(div2);     
 }
+
 
 
 // algorithm to find winner and compare choices of player and computer
@@ -45,54 +59,97 @@ const playGame = (player) => {
         if(computerChoice < 1/3 && computerChoice > 0) {
             computerChoice = 'rock';
             comIcon(player, computerChoice);
+            tie.innerText = `Ties: ${++tiecount}`;
             console.log("tie");
         }else if(computerChoice <2/3 && computerChoice > 1/3){
             computerChoice = 'paper';
             comIcon(player, computerChoice);
+            lose.innerText = `Loses: ${++losecount}`;
+            computerScore.innerText = `Computer Score: ${score.compcount += 1}`;  
+            localStorage.setItem('score', JSON.stringify(score));
             console.log("You lose");
         }else if(computerChoice < 1 && computerChoice > 2/3){
             computerChoice = 'scissors';
             comIcon(player, computerChoice);
+            win.innerText = `Wins: ${++wincount}`;
+            yourScore.innerText = `Your Score: ${score.playercount += 1}`;
+            localStorage.setItem('score', JSON.stringify(score));
             console.log("win");
         }
     }else if(player === 'paper'){
         if(computerChoice < 1/3 && computerChoice > 0) {
             computerChoice = 'rock';
             comIcon(player, computerChoice);
+            win.innerText = `Wins: ${++wincount}`;
+            yourScore.innerText = `Your Score: ${score.playercount += 1}`;
+            localStorage.setItem('score', JSON.stringify(score));
             console.log("You Win");
         }else if(computerChoice <2/3 && computerChoice > 1/3){
             computerChoice = 'paper';
             comIcon(player, computerChoice);
+            tie.innerText = `Ties: ${++tiecount}`;
             console.log("tie");
         }else if(computerChoice < 1 && computerChoice > 2/3){
             computerChoice = 'scissors';
             comIcon(player, computerChoice);
+            lose.innerText = `Loses: ${++losecount}`;
+            computerScore.innerText = `Computer Score: ${score.compcount += 1}`;
+            localStorage.setItem('score', JSON.stringify(score));
             console.log("You lose");
         }
     }else if(player === 'scissors'){
         if(computerChoice < 1/3 && computerChoice > 0) {
             computerChoice = 'rock';
             comIcon(player, computerChoice);
+            lose.innerText = `Loses: ${++losecount}`;
+            computerScore.innerText = `Computer Score: ${score.compcount += 1}`;
+            localStorage.setItem('score', JSON.stringify(score));
             console.log("You lose");
         }else if(computerChoice <2/3 && computerChoice > 1/3){
             computerChoice = 'paper';
             comIcon(player, computerChoice);
+            win.innerText = `Wins: ${++wincount}`;
+            yourScore.innerText = `Your Score: ${score.playercount += 1}`;
+            localStorage.setItem('score', JSON.stringify(score));
             console.log("win");
         }else if(computerChoice < 1 && computerChoice > 2/3){
             computerChoice = 'scissors';
             comIcon(player, computerChoice);
+            tie.innerText = `Ties: ${++tiecount}`;
             console.log("tie");
         }
     }
+    
 }
 
-const restartGame = () => {
+
+
+const restartGame = (val) => {
+    if(val){
+        wincount = 0; losecount = 0; tiecount = 0;
+        win.innerText = `Wins: ${wincount}`;
+        lose.innerText = `Loses: ${losecount}`;
+        tie.innerText = `Ties: ${tiecount}`;
+        chosen.innerHTML = '';
+    }
+    if(!val){
+        wincount = 0; losecount = 0; tiecount = 0;
+        win.innerText = `Wins: ${wincount}`;
+        lose.innerText = `Loses: ${losecount}`;
+        tie.innerText = `Ties: ${tiecount}`;
+        yourScore.innerText = `Your Score: ${score.playercount = 0}`;
+        computerScore.innerText = `Computer Score: ${score.compcount = 0}`;
+        localStorage.removeItem('score');
+        chosen.innerHTML = '';
+        
+    }
     console.log("0");
 }
 
 
 
-const btnRock = document.querySelector('.btnRock').addEventListener('click', () => playGame('rock'));
-const btnPaper = document.querySelector('.btnPaper').addEventListener('click', () => playGame('paper'));
-const btnScissors = document.querySelector('.btnScissors').addEventListener('click', () => playGame('scissors'));
-const btnRestart = document.querySelector('.btnRestart').addEventListener('click', restartGame);
+document.querySelector('.btnRock').addEventListener('click', () => playGame('rock'));
+document.querySelector('.btnPaper').addEventListener('click', () => playGame('paper'));
+document.querySelector('.btnScissors').addEventListener('click', () => playGame('scissors'));
+document.querySelector('.btnRestart').addEventListener('click', () => restartGame(true));
+document.querySelector('.btnReset').addEventListener('click', () => restartGame(false));
